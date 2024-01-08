d05e17c27d7b8904060866fbd9bb0492d7cd70aa
simpsonman <lng515@naver.com>
2024-01-08 18:13:07 +0900

    make project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  // mounted() {
+  //   console.log('hello : ', this.hello);
+  // },
+});
+</script>
