076b612822b6baa3d91a8dc004a1baeacf931f33
simpsonman <lng515@naver.com>
2024-02-04 17:46:52 +0900

    섹션7까지 수강완료

--- src/layouts/youtube/YoutubeHeader.vue
+++ src/layouts/youtube/YoutubeHeader.vue
@@ -71,6 +71,9 @@ const isSearchFocus = ref(false);
 <style lang="scss" scoped>
 .q-toolbar {
   height: 56px;
+  border-style: solid;
+  border-width: 0 0 1px 0;
+  border-color: #c9c9c9;
 }
 
 .search {
